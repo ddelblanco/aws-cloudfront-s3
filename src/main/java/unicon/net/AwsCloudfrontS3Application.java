a97a80ea49e5bc42246a1ae0d1185def650b007e
ddelblanco <diegodelblanco.ingeniero@gmail.com>
2017-03-20 13:17:35 -0700

    First commit. Base tool

---
+++ src/main/java/unicon/net/AwsCloudfrontS3Application.java
@@ -0,0 +1,12 @@
+package unicon.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCloudfrontS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCloudfrontS3Application.class, args);
+	}
+}
---
+++ src/test/java/unicon/net/AwsCloudfrontS3ApplicationTests.java
@@ -0,0 +1,16 @@
+package unicon.net;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsCloudfrontS3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
