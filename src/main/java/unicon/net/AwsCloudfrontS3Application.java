e5206930c2ac2fcba7ecfcb21f63fef6d5ff6aa6
ddelblanco <diegodelblanco.ingeniero@gmail.com>
2017-03-20 18:06:19 -0700

    v1.b

--- src/main/java/unicon/net/AwsCloudfrontS3Application.java
+++ src/main/java/unicon/net/AwsCloudfrontS3Application.java
@@ -2,11 +2,20 @@ package unicon.net;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsCloudfrontS3Application {
+public class AwsCloudfrontS3Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AwsCloudfrontS3Application.class);
+    }
+
+
+    public static void main(String[] args) {
 		SpringApplication.run(AwsCloudfrontS3Application.class, args);
 	}
 }
